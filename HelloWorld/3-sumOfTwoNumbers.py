3807b1ac5164a4b2c657d2eda26adffb5d9ae115
Vijay Venkata Santosh Kumar <VijayVenkataSantosh_Kumar@neimanmarcus.com>
2022-08-18 23:12:42 -0500

    added extra description in sun of two numbers

--- HelloWorld/3-sumOfTwoNumbers.py
+++ HelloWorld/3-sumOfTwoNumbers.py
@@ -1,4 +1,4 @@
 first_number = float(input("enter first number - "))
 second_number = float(input("enter second number - "))
 Sum = first_number * second_number
-print("Sum is : " + str(Sum))
+print("Sum is addition of two numbers: " + str(Sum))
